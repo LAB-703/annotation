2ff990451cf1227af74abead7ec6e50128e24236
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-06-27 03:27:22 +0900

    Add files via upload

--- jounal_annotation(beta1.0).py
+++ jounal_annotation(beta1.0).py
@@ -178,9 +178,26 @@ if select_event == "👀 기사 인용 도우미":
     if "emoji" not in st.session_state:
         st.session_state.emoji = "👈"
 
-    emojis = ["🐶", "🐱", "🐭", "🐹", "🐰", "🦊", "🐻", "🐼"]
+    emojis = ["💖","🧡","💛","💚","💙","💜","🤎","🖤"]
 
-    st.button(f"Click Me {st.session_state.emoji}", on_click=random_emoji)
+    st.button(f" 좋아요 {st.session_state.emoji}", on_click=random_emoji)
+    
+    st.title('Counter Example')
+    if 'count' not in st.session_state:
+        st.session_state.count = 0
+        st.session_state.last_updated = datetime.time(0,0)
+
+    def update_counter():
+        st.session_state.count += st.session_state.increment_value
+        st.session_state.last_updated = st.session_state.update_time
+
+    with st.form(key='my_form'):
+        st.time_input(label='Enter the time', value=datetime.datetime.now().time(), key='update_time')
+        st.number_input('Enter a value', value=0, step=1, key='increment_value')
+        submit = st.form_submit_button(label='Update', on_click=update_counter)
+
+    st.write('Current Count = ', st.session_state.count)
+    st.write('Last Updated = ', st.session_state.last_updated)
 
 #page2#######################################################################################################     
 if select_event == "📜 학술지 목록":
