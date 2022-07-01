a4becc2aaa562e6ee4a5f1c3829601740d2263a4
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-01 20:36:28 +0900

    Add files via upload

--- jounal_annotation(beta1.0).py
+++ jounal_annotation(beta1.0).py
@@ -259,7 +259,7 @@ conn = connect(credentials=credentials)
 # Uses st.cache to only rerun when the query changes or after 10 min.
 @st.cache(ttl=5)
 def run_query(query):
-    rows = conn.execute(query, headers=2)
+    rows = conn.execute(query, headers=1)
     rows = rows.fetchall()
     return rows
 
