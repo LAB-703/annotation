996a1e0da025f1183b78d63d15892e5b9c7482ff
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-01 20:24:19 +0900

    Add files via upload

--- jounal_annotation(beta1.0).py
+++ jounal_annotation(beta1.0).py
@@ -268,4 +268,5 @@ rows = run_query(f'SELECT * FROM "{sheet_url}"')
 
 # Print results.
 for row in rows:
-    st.write(f"{row.name} has a :{row.pet}:")
+    st.write(row)
+    #st.write(f"{row.name} has a :{row.pet}:")
