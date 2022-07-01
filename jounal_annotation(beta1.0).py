22d16819819e5d140b92a8b51c4f3d567cbfc833
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-01 21:43:53 +0900

    Add files via upload

--- jounal_annotation(beta1.0).py
+++ jounal_annotation(beta1.0).py
@@ -244,30 +244,32 @@ if select_event == "📌 개발":
 
 ####################
 
-#     from google.oauth2 import service_account
-#     from gsheetsdb import connect
+    from google.oauth2 import service_account
+    from gsheetsdb import connect
 
-#     # Create a connection object.
-#     credentials = service_account.Credentials.from_service_account_info(
-#         st.secrets["gcp_service_account"],
-#         scopes=[
-#             "https://www.googleapis.com/auth/spreadsheets",
-#         ],
-#     )
-#     conn = connect(credentials=credentials)
+    # Create a connection object.
+    credentials = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[
+            "https://www.googleapis.com/auth/spreadsheets",
+        ],
+    )
+    conn = connect(credentials=credentials)
 
-#     # Perform SQL query on the Google Sheet.
-#     # Uses st.cache to only rerun when the query changes or after 10 min.
-#     @st.cache(ttl=5)
-#     def run_query(query):
-#         rows = conn.execute(query, headers=1)
-#         rows = rows.fetchall()
-#         return rows
+    # Perform SQL query on the Google Sheet.
+    # Uses st.cache to only rerun when the query changes or after 10 min.
+    @st.cache(ttl=5)
+    def run_query(query):
+        rows = conn.execute(query, headers=1)
+        rows = rows.fetchall()
+        return rows
 
-#     sheet_url = st.secrets["private_gsheets_url"]
-#     rows = run_query(f'SELECT * FROM "{sheet_url}"')
+    sheet_url = st.secrets["private_gsheets_url"]
+    rows = run_query(f'SELECT * FROM "{sheet_url}"')
+    
+    st.rows
 
-#     # Print results.
-#     for row in rows:
-#         st.write(row)
-#         #st.write(f"{row.name} has a :{row.pet}:")
+    # Print results.
+    for row in rows:
+        st.write(row)
+        #st.write(f"{row.name} has a :{row.pet}:")
