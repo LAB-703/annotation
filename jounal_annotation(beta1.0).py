55495592aca9d1c4bcd1c3858e32ffa487aa2e9e
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-02 13:14:59 +0900

    Add files via upload

--- jounal_annotation(beta1.0).py
+++ jounal_annotation(beta1.0).py
@@ -324,12 +324,12 @@ if select_event == "📌 개발":
                         👉 QR코드를 스캔하거나 클릭시 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Toss-logo.svg/800px-Toss-logo.svg.png" height=20px>로 연결됩니다.<br> </p>''', unsafe_allow_html=True)
     st.markdown("---")
     st.header("📆 개발 기록")
-    st.markdown('<p align="right" style="color:#C0504D;">`📌 기능 추가` `🐞 버그 수정`</p>')
+    st.markdown('`📌 기능 추가` `🐞 버그 수정`')
     beta1_0=st.expander("1️⃣ 2022. 06. 28. beta 1.0 배포")
-    beta1_0.markdown('''<p align="left" style="font-size: 70%; text-indent : 20px;"> 📌 네이버/다음 뉴스 APA, CHICAGO 스타일 인용 기능 추가</p>''', unsafe_allow_html=True)
+    beta1_0.markdown('''<p align="left" style="font-size: 100%; text-indent : 20px;"> 📌 네이버/다음 뉴스 APA, CHICAGO 스타일 인용 기능 추가</p>''', unsafe_allow_html=True)
     beta2_0=st.expander("2️⃣ 2022. 07. 02. beta 2.0 배포")
     beta2_0.markdown('''<p align="left" style="font-size: 70%; text-indent : 20px;"> 🐞 최종검색일 타임존 UTC → KST 수정 </p>''', unsafe_allow_html=True)
-    beta2_0.markdown('''<p align="left" style="font-size: 70%; text-indent : 20px;"> 📌 학술지 페이지 오픈 🆕 새로운 학술지 추가에 동참해주세요! </p>''', unsafe_allow_html=True)
+    beta2_0.markdown('''<p align="left" style="font-size: 70%; text-indent : 20px;"> 📌 학술지 페이지 오픈 `new!` 새로운 학술지 추가에 동참해주세요! </p>''', unsafe_allow_html=True)
     beta2_0.markdown('''<p align="left" style="font-size: 70%; text-indent : 20px;"> 📌 개발자 커피 후원 기능 추가 </p>''', unsafe_allow_html=True)
     
     
