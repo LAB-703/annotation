234a971e55c6555b780d5b20d8899cd399660ded
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-02 22:08:20 +0900

    Add files via upload

--- article_annotation.py
+++ article_annotation.py
@@ -183,7 +183,7 @@ if "emoji" not in st.session_state:
     st.session_state.emoji = "🤍"
 likes=st.sidebar.button(f" 좋아요 {st.session_state.emoji}", on_click=random_emoji)
 gsheet_connector = connect_to_gsheet()
-likes_cnt=st.sidebar.markdown(get_data(gsheet_connector)['좋아요'][1])
+#likes_cnt=st.sidebar.markdown(get_data(gsheet_connector)['좋아요'][1])
 #if likes:
 #    likes=st.sidebar.button(f" 좋아요 {st.session_state.emoji}", on_click=random_emoji)
 
@@ -202,14 +202,13 @@ if select_event == "👀 기사 인용 도우미":
     with col2:
         if STYLE=="by JOURNAL":
             #st.markdown('<p style=" font-size: 100%; color:silver"> ⏳개발 중', unsafe_allow_html=True)
-            gsheet_connector = connect_to_gsheet()
 #             st.markdown("""<style>
 # div.st-be.st-bf.st-by.st-bz.st-c0.st-b4.st-c1.st-c2.st-bg.st-c3.st-c4.st-c5.st-c6 {visibility: hidden;}
 # div.st-be.st-bf.st-by.st-bz.st-c0.st-b4.st-c1.st-c2.st-bg.st-c3.st-c4.st-c5.st-c6:before {content: "찾으시는 학술지가 있나요?"; visibility: visible;}
 # </style>
 # """, unsafe_allow_html=True)
             gsheet_connector = connect_to_gsheet()
-            option = st.selectbox('찾으시는 학술지가 있나요?',list(get_data(gsheet_connector)['학술지']))
+#            option = st.selectbox('찾으시는 학술지가 있나요?',list(get_data(gsheet_connector)['학술지']))
             st.markdown('<p style=" font-size: 90%; color:silver"> 학술지가 없다면, 📜 학술지 목록 페이지에서 추가에 동참해 주세요.</p>', unsafe_allow_html=True)
     final_search=st.checkbox('최종 검색일(오늘) 추가')
     submit=st.button('인용')        
@@ -272,7 +271,7 @@ if select_event == "📜 학술지 목록":
     #st.subheader("⏳ 개발 중")
     st.markdown('<p align="center" style=" font-size: 140%;"><b>📜 등재된 학술지 목록</b></p>', unsafe_allow_html=True)
     gsheet_connector = connect_to_gsheet()
-    journal_list = st.selectbox('',list(get_data(gsheet_connector)['학술지']))
+#    journal_list = st.selectbox('',list(get_data(gsheet_connector)['학술지']))
     st.write("---")
     st.write(" ")
     expander = st.expander("학술지 추가를 원하신다면 클릭하세요.")
@@ -312,9 +311,9 @@ if select_event == "📜 학술지 목록":
             expander.error('❗ 학술지 한글 명칭을 입력해 주세요.')
             st.stop()
         else:    
-            add_row_to_gsheet(
-                gsheet_connector,
-                [[journal, annotation,TODAY]],
+  #          add_row_to_gsheet(
+  #              gsheet_connector,
+  #              [[journal, annotation,TODAY]],
             )
             expander.success("추가되었습니다! 👀 기사 인용 도우미 페이지에서 확인할 수 있습니다.")
             expander.balloons()
