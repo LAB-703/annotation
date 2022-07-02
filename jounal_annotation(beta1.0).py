435e54575683ab2c299d366df669131d33819ae0
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-02 13:36:29 +0900

    Add files via upload

--- jounal_annotation(beta1.0).py
+++ jounal_annotation(beta1.0).py
@@ -188,7 +188,7 @@ likes=st.sidebar.button(f" 좋아요 {st.session_state.emoji}", on_click=random_
 
 #page1#######################################################################################################
 if select_event == "👀 기사 인용 도우미":
-    st.markdown('<p align="center" style=" font-size: 120%;"><b>👀 척척 석박들을 위한 기사 인용 도우미</b></p>', unsafe_allow_html=True)
+    st.markdown('<p align="center" style=" font-size: 140%;"><b>👀 척척 석박들을 위한 기사 인용 도우미</b></p>', unsafe_allow_html=True)
 
     URL=st.text_input("네이버/다음 뉴스 url을 입력해주세요.")
     col1,col2=st.columns([5,5])  
@@ -318,16 +318,16 @@ if select_event == "📜 학술지 목록":
             expander.balloons()
 #page3#######################################################################################################
 if select_event == "📌 개발":
-    st.subheader("👩🏻‍💻 개발자")
+    st.markdown('<p align="left" style=" font-size: 140%;"><b>👩🏻‍💻 개발자</b></p>', unsafe_allow_html=True)
     col1,col2=st.columns([3,7])
     with col1 :
         st.markdown('<a href="http://m.site.naver.com/0Z7nr"><img src="https://qrcodethumb-phinf.pstatic.net/20220702_173/1656698540984dDVVY_PNG/0Z7nr.png"/></a>', unsafe_allow_html=True)
     with col2 :
         st.markdown('''<p align="left" style="font-size: 90%;">☕️ 개발자에게 커피 한잔은 큰 힘이 됩니다<br><br>
                         🎭 후원은 익명으로 가능합니다<br> <br>
-                        👉 QR코드를 스캔하거나 클릭시 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Toss-logo.svg/800px-Toss-logo.svg.png" height=15px>로 연결됩니다.<br> </p>''', unsafe_allow_html=True)
+                        👉 QR코드를 스캔하거나 클릭시 <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/30/Toss-logo.svg/800px-Toss-logo.svg.png" height=15px>로 연결됩니다<br> </p>''', unsafe_allow_html=True)
     st.markdown("---")
-    st.subheader("📆 개발 기록")
+    st.markdown('<p align="left" style=" font-size: 140%;"><b>📆 개발 기록</b></p>', unsafe_allow_html=True)
     st.markdown('<code>📌 기능 추가</code> <code>🐞 버그 수정</code>', unsafe_allow_html=True)
     beta1_0=st.expander("1️⃣ 2022. 06. 28. beta 1.0 배포")
     beta1_0.markdown('''<p align="left" style="font-size: 70%; text-indent : 20px;"> 📌 네이버/다음 뉴스 APA, CHICAGO 스타일 인용 기능 추가</p>''', unsafe_allow_html=True)
