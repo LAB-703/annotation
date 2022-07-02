0d437fbac553612011cf0f601dcfb700954a863b
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-02 15:21:52 +0900

    Add files via upload

--- jounal_annotation(beta1.0).py
+++ jounal_annotation(beta1.0).py
@@ -181,12 +181,15 @@ code {
 </style>
 '''
 st.markdown(hide_menu, unsafe_allow_html=True)
+gsheet_connector = connect_to_gsheet()
 
 select_event = st.sidebar.selectbox("🎈", ("👀 기사 인용 도우미", "📜 학술지 목록","📌 개발"))
 if "emoji" not in st.session_state:
     st.session_state.emoji = "🤍"
-likes=st.sidebar.button(f" 좋아요 {st.session_state.emoji}", on_click=likes)
-gsheet_connector = connect_to_gsheet()
+likes=st.sidebar.button(f" 좋아요 {st.session_state.emoji}", on_click=likes(
+                gsheet_connector,
+                [['좋아요']],
+            ))
 likes_cnt=st.sidebar.markdown(get_data(gsheet_connector)['좋아요'][1])
 #if likes:
 #    likes=st.sidebar.button(f" 좋아요 {st.session_state.emoji}", on_click=random_emoji)
