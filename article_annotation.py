0519b387364e3b1928311d55c909c466cbc06a94
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-05 01:05:10 +0900

    Add files via upload

--- article_annotation.py
+++ article_annotation.py
@@ -226,13 +226,14 @@ if select_event=="new":
 
     import gspread
     from oauth2client.service_account import ServiceAccountCredentials
-    scope = [
+    SCOPE = [
     'https://spreadsheets.google.com/feeds',
     'https://www.googleapis.com/auth/drive',
     ]
-    json_file_name = 'script/기사인용/instant-diorama-355010-d3d924c22704.json'
-    credentials = ServiceAccountCredentials.from_json_keyfile_name(json_file_name, scope)
-    gc = gspread.authorize(credentials)
+    credentials = ServiceAccountCredentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=[SCOPE],
+    )
     spreadsheet_url = 'https://docs.google.com/spreadsheets/d/1grpTFuy11UDNnqLVxiD3JyY24t7H8DMS-eAznhG43hU/edit#gid=0'
     # 스프레스시트 문서 가져오기 
     doc = gc.open_by_url(spreadsheet_url)
