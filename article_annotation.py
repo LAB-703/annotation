48ccb9885eb4285a24b197286db997f0d2d4dbee
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-05 02:12:17 +0900

    Add files via upload

--- article_annotation.py
+++ article_annotation.py
@@ -24,78 +24,6 @@ from googleapiclient.discovery import build
 from googleapiclient.http import HttpRequest
 
 from googleapiclient import discovery
-##################################################################
-import google_auth_httplib2
-import httplib2
-import pandas as pd
-import streamlit as st
-from google.oauth2 import service_account
-from googleapiclient.discovery import build
-from googleapiclient.http import HttpRequest
-
-from googleapiclient import discovery
-
-SCOPE = "https://www.googleapis.com/auth/spreadsheets"
-SPREADSHEET_ID = "1Ym2nbTDvApMRUErsPoT4frr_-6TAZY2gzrX2sfgaWLg"
-SHEET_NAME = "Database"
-GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
-
-#https://docs.google.com/spreadsheets/d/1Ym2nbTDvApMRUErsPoT4frr_-6TAZY2gzrX2sfgaWLg/edit?usp=sharing
-@st.experimental_singleton()
-def connect_to_gsheet():
-    # Create a connection object.
-    credentials = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"],
-        scopes=[SCOPE],
-    )
-
-    # Create a new Http() object for every request
-    def build_request(http, *args, **kwargs):
-        new_http = google_auth_httplib2.AuthorizedHttp(
-            credentials, http=httplib2.Http()
-        )
-        return HttpRequest(new_http, *args, **kwargs)
-
-    authorized_http = google_auth_httplib2.AuthorizedHttp(
-        credentials, http=httplib2.Http()
-    )
-    service = build(
-        "sheets",
-        "v4",
-        requestBuilder=build_request,
-        http=authorized_http,
-    )
-    service = discovery.build('sheets', 'v4', credentials=credentials)
-    gsheet_connector = service.spreadsheets()
-    return gsheet_connector
-
-
-def get_data(gsheet_connector) -> pd.DataFrame:
-    values = (
-        gsheet_connector.values()
-        .get(
-            spreadsheetId=SPREADSHEET_ID,
-            range=f"{SHEET_NAME}!A:E",
-        )
-        .execute()
-    )
-
-    df = pd.DataFrame(values["values"])
-    df.columns = df.iloc[0]
-    df = df[1:]
-    return df
-
-def add_row_to_gsheet(gsheet_connector, row) -> None:
-    gsheet_connector.values().append(
-        spreadsheetId=SPREADSHEET_ID,
-        range=f"{SHEET_NAME}!A:E",
-        body=dict(values=row),
-        valueInputOption="USER_ENTERED",
-    ).execute()
-
-
-
-#############################################################
 
 
 SCOPE = "https://www.googleapis.com/auth/spreadsheets"
@@ -420,7 +348,73 @@ if select_event == "📌 개발":
 
 #####################
 if select_event=="new":
+    import google_auth_httplib2
+    import httplib2
+    import pandas as pd
+    import streamlit as st
+    from google.oauth2 import service_account
+    from googleapiclient.discovery import build
+    from googleapiclient.http import HttpRequest
 
+    from googleapiclient import discovery
+
+    SCOPE = "https://www.googleapis.com/auth/spreadsheets"
+    SPREADSHEET_ID = "1Ym2nbTDvApMRUErsPoT4frr_-6TAZY2gzrX2sfgaWLg"
+    SHEET_NAME = "Database"
+    GSHEET_URL = f"https://docs.google.com/spreadsheets/d/{SPREADSHEET_ID}"
+
+    #https://docs.google.com/spreadsheets/d/1Ym2nbTDvApMRUErsPoT4frr_-6TAZY2gzrX2sfgaWLg/edit?usp=sharing
+    @st.experimental_singleton()
+    def connect_to_gsheet():
+        # Create a connection object.
+        credentials = service_account.Credentials.from_service_account_info(
+            st.secrets["gcp_service_account"],
+            scopes=[SCOPE],
+        )
+
+        # Create a new Http() object for every request
+        def build_request(http, *args, **kwargs):
+            new_http = google_auth_httplib2.AuthorizedHttp(
+                credentials, http=httplib2.Http()
+            )
+            return HttpRequest(new_http, *args, **kwargs)
+
+        authorized_http = google_auth_httplib2.AuthorizedHttp(
+            credentials, http=httplib2.Http()
+        )
+        service = build(
+            "sheets",
+            "v4",
+            requestBuilder=build_request,
+            http=authorized_http,
+        )
+        service = discovery.build('sheets', 'v4', credentials=credentials)
+        gsheet_connector = service.spreadsheets()
+        return gsheet_connector
+
+
+    def get_data(gsheet_connector) -> pd.DataFrame:
+        values = (
+            gsheet_connector.values()
+            .get(
+                spreadsheetId=SPREADSHEET_ID,
+                range=f"{SHEET_NAME}!A:E",
+            )
+            .execute()
+        )
+
+        df = pd.DataFrame(values["values"])
+        df.columns = df.iloc[0]
+        df = df[1:]
+        return df
+
+    def add_row_to_gsheet(gsheet_connector, row) -> None:
+        gsheet_connector.values().append(
+            spreadsheetId=SPREADSHEET_ID,
+            range=f"{SHEET_NAME}!A:E",
+            body=dict(values=row),
+            valueInputOption="USER_ENTERED",
+        ).execute()
 
     gsheet_connector = connect_to_gsheet()
     form = st.form(key="annotation")
