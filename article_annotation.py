eb4c4c60c8426899bf7a724337652ef00221fc6a
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-05 13:12:27 +0900

    Add files via upload

--- article_annotation.py
+++ article_annotation.py
@@ -131,8 +131,6 @@ if "emoji" not in st.session_state:
 ###################################
 select_event = st.sidebar.selectbox("🎈", ("👀 기사 인용 도우미", "📜 학술지 목록","📌 개발"))
 likes=st.sidebar.button(f" 좋아요 {st.session_state.emoji}", on_click=random_emoji)
-gsheet_connector = connect_to_gsheet()
-
 #likes_cnt=st.sidebar.markdown(get_data(gsheet_connector)['좋아요'][1])
 #if likes:
 #    likes=st.sidebar.button(f" 좋아요 {st.session_state.emoji}", on_click=random_emoji)
