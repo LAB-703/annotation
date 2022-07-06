7d0ba48d39cda41a061ae06902c4b53c5c3d285f
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-06 13:07:19 +0900

    Add files via upload

--- article_annotation.py
+++ article_annotation.py
@@ -13,6 +13,7 @@ from google.oauth2 import service_account
 from googleapiclient.discovery import build
 from googleapiclient.http import HttpRequest
 from pytz import timezone
+from googleapiclient import discovery
 from gsheetsdb import connect
 
 #def likes(gsheet_connector, row) -> None:
