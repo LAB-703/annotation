c2b2b3714baafe2b53aa8421466313b116711e5e
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-10 05:30:06 +0900

    Update article_annotation.py

--- article_annotation.py
+++ article_annotation.py
@@ -169,7 +169,7 @@ if "emoji" not in st.session_state:
 #select_event = st.sidebar.selectbox("🎈", ("👀 기사 인용 도우미", "📜 학술지 목록","📌 개발", "⏳ 개발중","개발"))
 likes=st.sidebar.button(f" 좋아요 {st.session_state.emoji}", on_click=random_emoji)
 # gsheet_connector = connect_to_gsheet()
-text=get_data(gsheet_connector)['좋아요'].count()+"명이 좋아합니다💖"
+text=str(get_data(gsheet_connector)['좋아요'].count())+"명이 좋아합니다💖"
 likes_cnt=st.sidebar.markdown(text)
 
 # if likes:
