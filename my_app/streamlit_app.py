004e20b69888fcc2af888f94c0118a02bafac26e
LAB-703 <92988910+LAB-703@users.noreply.github.com>
2022-07-10 02:59:27 +0900

    Update streamlit_app.py

--- my_app/streamlit_app.py
+++ my_app/streamlit_app.py
@@ -14,9 +14,9 @@ def page3():
     st.sidebar.markdown("# Page 3 🎉")
 
 page_names_to_funcs = {
-    "Main Page": main_page,
-    "Page 2": page2,
-    "Page 3": page3,
+    "👀 기사 인용 도우미": main_page,
+    "📜 학술지 목록": page2,
+    "📌 개발": page3,
 }
 
 #selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
